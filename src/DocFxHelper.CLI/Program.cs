e40d7b66c54940d45ded17064878a17fd065f37e
Patrice Calvé <Patrice.Calve@parl.gc.ca>
2025-02-15 13:45:21 -0500

    Make configuration loading settings from exe's location

--- src/DocFxHelper.CLI/Program.cs
+++ src/DocFxHelper.CLI/Program.cs
@@ -1,6 +1,5 @@
 // See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NLog.Extensions.Logging;
@@ -8,21 +7,39 @@ using System.CommandLine;
 using System.ComponentModel.Design;
 using System.Text.Json;
 using DocFxHelper.CLI.Commands;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("DocFxHelper starting");
+var ass = System.Reflection.Assembly.GetExecutingAssembly();
+var exe = new FileInfo(ass.Location);
 
-var builder = Host.CreateApplicationBuilder(args);
+Console.WriteLine("DocFxHelper CLI [{0}]", ass.GetName().Version);
 
-builder.Services.Configure<DocFxHelper.CLI.Settings.DocFxHelperSettings>(builder.Configuration.GetSection(DocFxHelper.CLI.Settings.DocFxHelperSettings.SectionName));
+var hostSettings = new HostApplicationBuilderSettings
+{
+  ContentRootPath = exe.Directory!.FullName,
+  Configuration = new ConfigurationManager()
+};
+
+var dfh_json = System.IO.Path.Combine(System.Environment.CurrentDirectory, "dfh.json");
+hostSettings.Configuration!.AddJsonFile(dfh_json, optional: true);
+hostSettings.Configuration!.AddEnvironmentVariables(prefix: "DFH_");
+hostSettings.Configuration!.AddCommandLine(args);
+
+var builder = Host.CreateApplicationBuilder(hostSettings);
+
+builder.Services.Configure<DocFxHelper.CLI.Settings.DocFxHelperSettings>(
+  builder.Configuration.GetSection(
+    DocFxHelper.CLI.Settings.DocFxHelperSettings.SectionName
+  )
+);
 
 builder.Services.RegisterCommands();
 
 builder.Logging.ClearProviders();
-var loggingBuilder = builder.Logging.AddNLog();
+builder.Logging.AddNLog();
 
 using IHost host = builder.Build();
-
-
 
 return await host.Services.RunRootCommandAsync(args);
 
